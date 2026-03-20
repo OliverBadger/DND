b808747b8dfe981c9e58e73a8d527ab5ef178585
Oliver Badger <oliverbadger@aol.com>
2026-03-20 20:59:50 +0000

    InputHandlerUpdate

--- Assets/_Scripts/InputHandler.cs
+++ Assets/_Scripts/InputHandler.cs
@@ -7,6 +7,13 @@ public class InputHandler : MonoBehaviour
 
     private void Awake()
     {
-        
+        _camera = Camera.main;
+    }
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        if(!context.started) return;
+
+        var rayHit = Physics2D.GetRayIntersection(_camera.ScreenPointToRay(Mouse.current.position.ReadValue()));
     }
 }
