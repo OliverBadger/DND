c39cfa884e3db22b473a9c1ad7244fbc5de84423
Oliver Badger <74118632+OliverBadger@users.noreply.github.com>
2026-03-21 12:36:31 +0000

    Refactor InputHandler to initialize player and bar components in Awake method

--- Assets/_Scripts/InputHandler.cs
+++ Assets/_Scripts/InputHandler.cs
@@ -4,10 +4,18 @@ using UnityEngine.InputSystem;
 public class InputHandler : MonoBehaviour
 {
     private Camera _camera;
+    private GameObject player;
+    private PlayerLocation playerLocation;
+    private GameObject bar;
+    private OpenBarHandler openBarHandler;
 
     private void Awake()
     {
         _camera = Camera.main;
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerLocation = player.GetComponent<PlayerLocation>();
+        bar = GameObject.FindGameObjectWithTag("Bar");
+        openBarHandler = bar.GetComponent<OpenBarHandler>();
     }
 
     public void OnClick(InputAction.CallbackContext context)
@@ -17,6 +25,10 @@ public class InputHandler : MonoBehaviour
         var rayHit = Physics2D.GetRayIntersection(_camera.ScreenPointToRay(Mouse.current.position.ReadValue()));
         if(!rayHit.collider) return;
 
-        Debug.Log(rayHit.collider.gameObject.name);
+        if(rayHit.collider.gameObject.CompareTag("Bar"))
+        {
+            playerLocation.locationString = "Bar";
+            openBarHandler.OpenBar();
+        }
     }
 }
